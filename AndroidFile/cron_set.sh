cdb9eb2495d604399a13d3842665a51039c056a9
雪狐 <mengshouer@outlook.com>
2024-06-19 16:17:30 +0800

    feat: update cron_set.sh desc

--- AndroidFile/cron_set.sh
+++ AndroidFile/cron_set.sh
@@ -16,18 +16,20 @@ not_kill_time_right=730
 # 定时任务数量，根据下面实际填写，可以多过实际数量，但是过多会导致运行时间变长
 cron_count=2
 
-# cron_config_pkg 和 cron_config_rule 一一对应，不要多也不要少
+# 所有的 cron_config_配置项 一一对应，不要多也不要少
 # cron_config_pkg 为包名/启动类名，前面添加 --user 999 则启动双开应用，有些多开软件 user 不是 999，需要自己查看
-# cron_config_rule 为 cron 的规则，例子： 0 */1 * * *  为每小时执行一次
 # 本质上就是执行 am start [cron_config_pkg] 命令，可以在这里添加自己的应用
-# 下面两个是支付宝的例子，可以根据自己的需求添加
+# cron_config_XXXX0 0结尾的为示例配置，修改了也不生效的。
+cron_config_pkg0="xxxx"
+# cron_config_rule 为 cron 的规则，例子： 0 */1 * * *  为每小时执行一次
+cron_config_rule0="0 */1 * * *"
+# 如果需要亮屏时不启动应用，可以在对应的配置后面添加 cron_config_screen_on_no_startX=true
+cron_config_screen_on_no_start0=true
+
+
+# 下面两个是支付宝的例子，可以根据自己的需求添加或删除
 cron_config_pkg1="com.eg.android.AlipayGphone/com.alipay.mobile.framework.service.common.SchemeStartActivity"
 cron_config_rule1="0 */1 * * *"
 
 cron_config_pkg2="--user 999 com.eg.android.AlipayGphone/com.alipay.mobile.framework.service.common.SchemeStartActivity"
 cron_config_rule2="0 */1 * * *"
-
-cron_config_pkg3="xxxx"
-cron_config_rule3="0 */1 * * *"
-# 如果需要亮屏时不启动应用，可以在对应的配置后面添加 cron_config_screen_on_no_startX=true
-cron_config_screen_on_no_start3=true
