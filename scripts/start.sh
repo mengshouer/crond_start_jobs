a5da93842c08939b3a4b116965facda1789e720a
mengshouer <mengshouer@outlook.com>
2025-08-19 21:16:41 +0800

    feat: 优化唤醒锁开关

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,18 +30,7 @@ start_inotifyd() {
 
 # 检查并启动inotifyd功能
 if [[ ! -f "${backup_dir}/onlycrond" ]]; then
-  logd "| inotifyd 已开启 |：当前可以使用 su 管理器(magisk/ksu/ap)内管理模块开关，不需要重启手机"
-  logd "此功能是用 inotifyd 监控模块目录实现的，不经常开关模块的可以关掉"
-  logd "如需关闭此功能，在 ${backup_dir} 中新建一个 onlycrond 文件，重启生效"
-  logd "如果是有操作按钮的新版 su 管理器，可以关闭此项，使用操作按钮控制"
   start_inotifyd
-fi
-
-# 检查并设置唤醒锁
-if [[ ! -f "${backup_dir}/nowakelock" ]]; then
-  echo "PowerManagerService.noSuspend" >> /sys/power/wake_lock
-  logd "|已开启设备唤醒锁，会增加耗电，关闭可能导致任务在设备休眠后无法准时执行"
-  logd "如需关闭此功能，在 ${backup_dir} 中新建一个 nowakelock 文件，重启生效"
 fi
 
 start_service
